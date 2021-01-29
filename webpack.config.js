63032d52ffcbaaba061151850e88ddcd6d8d99ef
MaxD <max.moderation@gmail.com>
2021-01-29 19:02:38 +0200

    add html plugin

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'script.js',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            },
+        })
+    ],
+
+    
+}
