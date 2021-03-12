5b7015144c9057ef31f016ede8e325f7bbb44986
MaxD <max.moderation@gmail.com>
2021-03-12 16:23:07 +0200

    adaptive header

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,22 @@
 import * as $ from 'jquery'
 import '../styles/styles.scss'
 
-// console.log('Hello webpack!')
+
+// ============= BURGER ================
+let menu = $('.burger-menu');
+let burgerButton = $('.burger-menu__button');
+let list = $('.burger-menu__list');
+
+burgerButton.on('click', () => toggleMenu());
+
+list.on('click', () => toggleMenu());
+
+function toggleMenu() {
+    menu.toggleClass('burger-active');
+
+    if (menu.hasClass('burger-active')) {
+        $('body').css('overflow', 'hidden');
+    } else {
+        $('body').css('overflow', 'visible');
+    }
+}
